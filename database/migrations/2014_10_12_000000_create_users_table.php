17f447f500964a5547f4d60c2b6de689517ddf54
= <ilhamkartolonggeng21@gmail.com>
2022-06-28 20:36:02 +0700

    membuat migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,8 +1,36 @@
+Skip to content
+Search or jump to…
+Pull requests
+Issues
+Marketplace
+Explore
+ 
+@ilhamlutfiii 
+mbemvieira
+/
+parking-api
+Public
+Code
+Issues
+Pull requests
+Actions
+Projects
+Wiki
+Security
+Insights
+parking-api/database/migrations/2014_10_12_000000_create_users_table.php /
+
+Mateus Vieira Now using Laravel
+Latest commit 4338282 on Jun 8, 2017
+ History
+ 0 contributors
+35 lines (32 sloc)  746 Bytes
+
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 class CreateUsersTable extends Migration
 {
@@ -14,10 +42,9 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
---
+++ database/migrations/2022_06_28_131041_create_vehicle_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVehicleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vehicles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('client_id')->unsigned();
+            $table->foreign('client_id')->references('id')->on('clients')->onDelete('cascade');
+            $table->integer('parking_place_id')->unsigned()->nullable();
+            $table->foreign('parking_place_id')->references('id')->on('parking_places');
+
+            $table->string('plate');
+            $table->unique(['plate', 'client_id']);
+            $table->string('brand');
+            $table->string('model');
+            $table->string('color')->nullable();
+            $table->string('year')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vehicles');
+    }
+}
